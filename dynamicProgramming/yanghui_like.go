48261e7da0239b358b722d865852e9925ea6f2e1
kakiezhang <782922086@qq.com>
2020-04-05 11:15:58 +0800

    path & idx

--- dynamicProgramming/yanghui_like.go
+++ dynamicProgramming/yanghui_like.go
@@ -1,6 +1,6 @@
 /**
 在一个类似杨辉的三角结构中，
-从顶到底求经过的最短路径和
+从顶到底求经过的最短路径&最小和
 行/列：三角的层级
 状态节点：初始化时，先将每一层的路径节点
 映射到states上，然后每一层直接根据上一层
@@ -8,7 +8,10 @@
 */
 package dynamicProgramming
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Yanghui struct {
 	arr    []int
@@ -40,7 +43,16 @@ func newYanghui(arr []int, level int) *Yanghui {
 	return yh
 }
 
-func (yh *Yanghui) FindMinPath() int {
+func (yh *Yanghui) PrintTriangle() {
+	for i := 0; i < yh.level; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Printf("%d ", yh.states[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func (yh *Yanghui) FindMinPath() ([]int, []int) {
 	for i := 1; i < yh.level; i++ { // 直接从第1行开始
 		for j := 0; j <= i; j++ {
 			var min = math.MaxInt64
@@ -54,12 +66,38 @@ func (yh *Yanghui) FindMinPath() int {
 		}
 	}
 
-	var min = math.MaxInt64
+	var sum = math.MaxInt64 // 最小路径和
+	var col int             // 最后一行最小路径所在的列
 	for i := 0; i < yh.level; i++ {
-		if min > yh.states[yh.level-1][i] {
-			min = yh.states[yh.level-1][i]
+		if sum > yh.states[yh.level-1][i] {
+			sum = yh.states[yh.level-1][i]
+			col = i
 		}
 	}
 
-	return min
+	idx := make([]int, yh.level) // 存储每一层的最短节点在states中的索引
+	idx[yh.level-1] = col
+
+	path := make([]int, yh.level) // 存储每一层的最短节点在states中的节点值
+	path[0] = yh.states[0][0]
+
+	// 最短路径，从倒数第二层开始，把上面的过程倒过来求一遍
+	for i := yh.level - 2; i >= 0; i-- {
+		var min = math.MaxInt64
+		var j int          // 当前行最小路径所在的列
+		var col = idx[i+1] // 当前行的下一层最小路径所在的列
+		if col > 0 {
+			j = idx[i+1] - 1
+			min = yh.states[i][j]
+		}
+		if col <= i && min > yh.states[i][col] {
+			j = col
+			min = yh.states[i][j]
+		}
+
+		idx[i] = j
+		path[i+1] = yh.states[i+1][col] - min
+	}
+
+	return idx, path
 }
--- dynamicProgramming/yanghui_like_test.go
+++ dynamicProgramming/yanghui_like_test.go
@@ -6,11 +6,18 @@ import (
 )
 
 func TestNewYanghui(t *testing.T) {
-	arr := []int{5, 7, 8, 2, 3, 4, 4, 9, 6, 1, 2, 7, 9, 4, 5}
+	var arr []int
+	arr = []int{5, 7, 8, 2, 3, 4, 4, 9, 6, 1, 2, 7, 9, 4, 5}
+	arr = []int{5, 7, 8, 5, 3, 4, 12, 9, 9, 10, 2, 7, 9, 4, 5}
 	yh := newYanghui(arr, 5)
-	fmt.Printf("%+v\n", yh)
+	fmt.Printf("Yanghui: %+v\n", yh)
 
-	minSum := yh.FindMinPath()
-	fmt.Printf("%+v\n", yh.states)
-	fmt.Printf("minSum: %d\n", minSum)
+	fmt.Println("Origin states: ")
+	yh.PrintTriangle()
+
+	idx, path := yh.FindMinPath()
+	fmt.Println("After states: ")
+	yh.PrintTriangle()
+	fmt.Printf("idx: %v\n", idx)
+	fmt.Printf("path: %v\n", path)
 }
